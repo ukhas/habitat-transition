eabc1b33e52a2b0723ed364327777b1c0736cf22
Daniel Richman <main@danielrichman.co.uk>
2012-08-26 23:35:50 +0100

    Initial attempt to update to latest schema

--- habitat_transition/spacenearus.py
+++ habitat_transition/spacenearus.py
@@ -87,11 +87,26 @@ class SpaceNearUs:
         logger.debug("Queue length now: " + str(self.upload_queue.qsize()))
 
     def payload_telemetry(self, doc):
+        required = {
+            # lat/lon are required. It's got to be somewhere, why not bermuda?
+
+            # seriously though: The only payloads that don't have lat/lon are
+            # OSIRIS and PETUNIA. There's an exception in spacenear.us to make
+            # the icons invisible and these coordinates ^ keep them out of the
+            # way (won't accidentally be clicked on, etc).
+            "lat": 32.3,
+            "lon": -64.8,
+            "alt": 0,
+            "time": "00:00:00",
+        }
+        # TODO
+
         fields = {
             "vehicle": "payload",
             "lat": "latitude",
             "lon": "longitude",
             "alt": "altitude",
+            "time": "time",
             "heading": "heading",
             "speed": "speed",
             "temp_inside": "temperature_internal",
@@ -133,10 +148,11 @@ class SpaceNearUs:
                 self.recent_doc_receivers[doc_id] = doc["receivers"].keys()
                 new_receivers = doc["receivers"].keys()
 
-        params = {}
+        params = copy.deepcopy(required)
 
         self._copy_fields(fields, data, params)
-        self._handle_time(data, params)
+        # format of time is HH:MM:SS, checked by validation
+        params["time"] = params["time"].replace(":", "")
 
         unused_data = {}
         used_keys = set(fields.values() + ["time"])
@@ -187,7 +203,10 @@ class SpaceNearUs:
         params = {}
 
         self._copy_fields(fields, data, params)
-        self._handle_time(data, params, doc["time_created"])
+
+        created = rfc3339.rfc3339_to_timestamp(doc["time_created"])
+        timestr = time.strftime("%H%M%S", time.gmtime(created))
+        params["time"] = timestr
 
         params["pass"] = "aurora"
         self.upload_queue.put(params)
@@ -218,21 +237,6 @@ class SpaceNearUs:
                 params[tgt] = data[src]
             except KeyError:
                 continue
-
-    def _handle_time(self, data, params, time_created=None):
-        if "time" not in data and time_created is None:
-            logger.warning("No time on doc")
-            return
-
-        if "time" not in data:
-            created = rfc3339.rfc3339_to_timestamp(time_created)
-            timestr = time.strftime("%H%M%S", time.gmtime(created))
-            params["time"] = timestr
-        elif isinstance(data["time"], dict):
-            timestr = "{hour:02d}{minute:02d}{second:02d}"
-            params["time"] = timestr.format(**data["time"])
-        else:
-            params["time"] = data["time"].replace(":", "")
 
     def _post_to_track(self, params):
         qs = urlencode(params, True)
